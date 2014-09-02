95ad6abb74d0f64d44a2c102421c77f387d9c373
Jurvis Tan <jurvis.tan@me.com>
2014-09-02 13:36:46 +0800

    added final copy

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,5 @@
 $( document ).ready(function() {
     new WOW().init();
     $('#biography').addClass('animated zoomInUp');
-    $('#section-right').addClass('animated bounceInRight');
-    $('#section-left').addClass('animated bounceInLeft');
+    $('#footer-btn').addClass('animated bounceIn');
 });
