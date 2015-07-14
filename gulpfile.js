5c04ebc540e124cecd4038bf0171cd0b3aef6fde
Jurvis Tan <jurvis.tan@me.com>
2015-07-14 22:21:08 +0800

    cleanup unused CSS

--- gulpfile.js
+++ gulpfile.js
@@ -24,7 +24,7 @@ gulp.task('dev', function() {
 gulp.task('serve', function() {
 	gulp.run('build');
 	browserSync({
-		proxy: "localhost:8888/jurvis-portfolio"
+		proxy: "localhost:8888/nebulo-landing"
 	});
 
 	gulp.watch([
